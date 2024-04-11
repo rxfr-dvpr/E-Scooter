3e0cd45d621276af3519fde0547b630c55ff8021
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-04-11 13:14:48 +0500

    project updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <section class="home-page">
+    <div class="container">
+      <div class="row">
+        <p>E-Scooter Soon...</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Home Page'
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+.home-page {
+  width: 100%;
+  height: 100vh;
+
+  .row {
+    justify-items: center;
+    align-items: center;
+
+    p {
+      width: 100%;
+      font-size: 35px;
+      font-weight: 600;
+      text-align: center;
+    }
+  }
+}
+
+
+</style>
