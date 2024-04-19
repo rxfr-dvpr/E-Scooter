b414952bb4f66eaaf902c0bec577c1a9e0ade357
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-04-19 21:48:14 +0500

    cards added in appearance section

--- src/components/Appearance.vue
+++ src/components/Appearance.vue
@@ -1,8 +1,18 @@
 <template>
-  <section class="appearance__section" :class="{'light': light}">
+  <section class="appearance__section" :class="{'light': light}" id="appearance">
     <div class="container">
         <div class="row">
             <h2 class="appearance-title">{{ title }}</h2>
+
+            <div class="appearance__cards">
+                <div class="appearance__cards-item" v-for="(card, idx) in cards" :key="idx">
+                    <img :src="card.img" alt="" class="card-img">
+
+                    <h4 class="card-title">{{ card.title }}</h4>
+
+                    <p class="card-txt">{{ card.txt }}</p>
+                </div>
+            </div>
 
             <img :src="filterImg" alt="" class="appearance-filter">
         </div>
@@ -23,7 +33,19 @@ export default {
     data() {
         return {
             title: 'Лаконичный современный дизайн, в котором нет ничего лишнего',
-            filterImg: 'https://firebasestorage.googleapis.com/v0/b/mi-scooter-2e744.appspot.com/o/Appearance%2Fappearance-filter.png?alt=media&token=fc1e567c-d2d6-442b-9767-1b309f439b53'
+            filterImg: 'https://firebasestorage.googleapis.com/v0/b/mi-scooter-2e744.appspot.com/o/Appearance%2Fappearance-filter.png?alt=media&token=fc1e567c-d2d6-442b-9767-1b309f439b53',
+            cards: [
+                {
+                    img: 'https://firebasestorage.googleapis.com/v0/b/mi-scooter-2e744.appspot.com/o/Appearance%2Fcard-1.png?alt=media&token=7e74fdf9-8c9e-410d-a9c4-5a695f44a907',
+                    title : 'Минималистичный дизайн',
+                    txt: 'Рама, компоненты и аксессуары скутера выдержаны в едином стиле, где удобство пользователя выходит на первое место.  Одного прикосновения достаточно, чтобы запустить самокат.'
+                },
+                {
+                    img: 'https://firebasestorage.googleapis.com/v0/b/mi-scooter-2e744.appspot.com/o/Appearance%2Fcard-2.png?alt=media&token=6656cfa6-1805-4d93-8c01-82efacaeb0d4',
+                    title : 'Удобная панель управления',
+                    txt: 'Удобно расположенные элементы управления. Отображение8 типов данных в реальном времени: скорость, режим, заряд аккумулятора, состояние блокировки и другая информация.'
+                }
+            ]
         }
     }
 }
@@ -58,9 +80,50 @@ export default {
             max-width: max-content;
             width: 100%;
             position: absolute;
-            top: -400%;
-            left: 50%;
+            top: -50%;
+            left: 40%;
             z-index: -1;
+        }
+
+        &__cards {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            gap: 40px;
+            flex-wrap: wrap;
+
+            &-item {
+                max-width: 560px;
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                row-gap: 15px;
+                position: relative;
+                padding: 400px 60px 45px;
+                overflow: hidden;
+                border-radius: 15px;
+                background: #0E0E0E;
+
+                .card-img {
+                    width: 100%;
+                    max-height: 100%;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    object-fit: cover;
+                }
+
+                .card-title, .card-txt {
+                    z-index: 2;
+                }
+                
+            }
+        }
+    }
+
+    &.light {
+        .appearance-title {
+            color: var(--main-black);
         }
     }
 }
